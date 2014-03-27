25fc25a8857770ffaa5fa08acd7b0c53b18b53ab
smihica <smihica@gmail.com>
2014-03-27 21:50:43 +0900

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ And the zip file is able to extract in WINDOWS.
 
 This is a simple Minizip wrapper of python.
 (http://www.winimage.com/zLibDll/minizip.html)
+
 This software uses zlib.
 License: zlib/libpng License.
 
@@ -54,7 +55,7 @@ install pyminizip
 
 ----------------------------------------------------------------------------
 
-Provides just one function.
+Provides two functions.
 ==============================
 
 pyminizip.compress("/srcfile/path.txt", "/distfile/path.zip", "password", int(compress_level))
@@ -67,6 +68,17 @@ pyminizip.compress("/srcfile/path.txt", "/distfile/path.zip", "password", int(co
 
   Return value:
   - always returns None
+
+pyminizip.compress_multiple([u'pyminizip.so', 'file2.txt'], "file.zip", "1233", 4)
+  Args:
+  1. src file LIST path (list)
+  2. dst file path (string)
+  3. password (string) or None (to create no-password zip)
+  4. compress_level(int) between 1 to 9, 1 (more fast)  <---> 9 (more compress)
+
+  Return value:
+  - always returns None
+
 
 ==============================
 """,
