1bee3452bdde62dadce3454aeed6afa0ede17553
Jeff Tang <mrjefftang@gmail.com>
2014-07-02 13:47:31 -0400

    Switch to setuptools to build wheels

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 SOURCES = ['src/py_minizip.c', 'src/zip.c', 'src/ioapi.c']
 LIBRARIES = ['z']
