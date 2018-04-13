0950a85321f175f7e0d8350ff0a5683214fb3bde
Andrea Baldini <a.baldini@teamsystem.com>
2018-04-13 09:57:15 +0200

    Updated README.txt. Set long_description reading file.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import os
 from setuptools import setup, Extension
 
 SOURCES = ['src/py_minizip.c', 'src/zip.c', 'src/py_miniunz.c', 'src/unzip.c', 'src/ioapi.c',
@@ -9,6 +10,9 @@ SOURCES = ['src/py_minizip.c', 'src/zip.c', 'src/py_miniunz.c', 'src/unzip.c', '
 
 if 'win32' in sys.platform:
     SOURCES.append('src/iowin32.c')
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = 'pyminizip',
@@ -36,60 +40,5 @@ setup(
                   include_dirs=['src','zlib123'],
                   )
         ],
-    long_description = """\
-To create a password encrypted zip file in python.
-And the zip file is able to extract in WINDOWS.
-
-This is a simple Minizip wrapper of python.
-(http://www.winimage.com/zLibDll/minizip.html)
-
-This software uses zlib.
-License: zlib/libpng License.
-
-install zlib
-
-    linux:
-    $ sudo apt-get install zlib
-    mac:
-    $ sudo port install zlib
-
-install pyminizip
-
-    $ pip install pyminizip
-
-----------------------------------------------------------------------------
-
-Provides three functions.
-==============================
-
-pyminizip.compress("/srcfile/path.txt", "/distfile/path.zip", "password", int(compress_level))
-
-  Args:
-  1. src file path (string)
-  2. dst file path (string)
-  3. password (string) or None (to create no-password zip)
-  4. compress_level(int) between 1 to 9, 1 (more fast) <---> 9 (more compress) or 0 (default)
-
-  Return value:
-  - always returns None
-
-pyminizip.compress_multiple([u'pyminizip.so', 'file2.txt'], "file.zip", "1233", 4)
-  Args:
-  1. src file LIST path (list)
-  2. dst file path (string)
-  3. password (string) or None (to create no-password zip)
-  4. compress_level(int) between 1 to 9, 1 (more fast)  <---> 9 (more compress)
-
-pyminizip.uncompress("/srcfile/path.zip", "password", "/dirtoextract", int(withoutpath))
-  1. src file path (string)
-  2. password (string) or None (to unzip encrypted archives)
-  3. dir path to extract files
-  4. withoutpath (exclude path of extracted)
-
-  Return value:
-  - always returns None
-
-
-==============================
-""",
+    long_description = read('README.txt'),
     )
